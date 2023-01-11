5eec7fe180def07b749de49e3d5bf7f7592260be
Mariana Petropoulou <marianapetropoulou@gmail.com>
2023-01-11 18:49:16 +0200

    :))

---
+++ GradingApp/Views/Home/TeacherHomepage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GradingApp.Views.Home
+{
+    public class TeacherHomepageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
