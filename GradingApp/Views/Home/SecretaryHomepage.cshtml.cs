69d61582f28c2b21f0b04c6ba648583c98004ec7
Mariana Petropoulou <marianapetropoulou@gmail.com>
2023-01-11 19:32:19 +0200

    secretary

---
+++ GradingApp/Views/Home/SecretaryHomepage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GradingApp.Views.Home
+{
+    public class SecretaryHomepageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
