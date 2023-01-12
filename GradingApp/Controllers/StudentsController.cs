c649904858bee575c6c75bd5435a708d75f8723b
PaliouraP <61277841+PaliouraP@users.noreply.github.com>
2023-01-12 16:26:07 +0200

    my card - see grades for all examined subjects

--- GradingApp/Controllers/StudentsController.cs
+++ GradingApp/Controllers/StudentsController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GradingApp.Data;
+using GradingApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GradingApp.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public StudentsController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GradesSubject(string Username)
+        {
+            ViewBag.Username = Username;
+            return View("GradesSubject");
+        }
+
+        public IActionResult GradesSemester(string Username)
+        {
+            ViewBag.Username = Username;
+            return View("GradesSemester");
+        }
+
+        public IActionResult MyCard(string Username)
+        {
+            ViewBag.Course = _db.Course.ToList();
+            ViewBag.Students = _db.Students.ToList();
+            ViewBag.CourseHasStudents = _db.CourseHasStudents.ToList();
+            ViewBag.Username = Username;
+            return View("MyCard");
         }
     }
 }
