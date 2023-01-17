42b56c7a8e293de2bc2215979ea0c4d3bda2a36b
Mariana Petropoulou <marianapetropoulou@gmail.com>
2023-01-18 01:03:31 +0200

    padding in tables, removed "0.5" from average, grade submit form UI, confirmation messages

---
+++ GradingApp/wwwroot/js/AssignmentSuccessScript.js
@@ -0,0 +1,10 @@
+﻿function success_student() {
+
+    alert('Student is assigned to course successfully!');
+
+}
+function success_professor() {
+
+    alert('Professor is assigned to course successfully!');
+
+}
---
+++ GradingApp/wwwroot/js/RegistrationSuccessScript.js
@@ -0,0 +1,6 @@
+﻿
+function success() {
+    
+        alert('Registration successful!');
+    
+}
